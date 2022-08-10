cc4e239ae58072c744879bc6c95469bc2ffe24b0
ohmin839 <ohmin839.echoda.resident@gmail.com>
2022-08-10 20:42:55 +0900

    Added minimal test cases

--- jsybuben-core/test/to_hayeren_word_dict.test.js
+++ jsybuben-core/test/to_hayeren_word_dict.test.js
@@ -4,6 +4,7 @@ const to_hayeren_word_dict = api.to_hayeren_word_dict;
 test.each([
     ["", {}],
     ["Բարև Ձեզ։", {"Բարև": 1, "Ձեզ": 1}],
+    ["Բարև Ձեզ։ Բարև ձեզ։", {"Բարև": 2, "Ձեզ": 1, "ձեզ": 1}],
 ])("from %s to %s", (text, splitted) => {
     expect(to_hayeren_word_dict(text)).toStrictEqual(splitted);
 });
