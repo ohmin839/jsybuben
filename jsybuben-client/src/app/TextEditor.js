16508048393ab10e41bffe1d54984a606ef5e471
ohmin839 <ohmin839.echoda.resident@gmail.com>
2022-09-14 21:10:48 +0900

    Modified TextEditor(in a widget-base manner)

--- jsybuben-client/src/app/TextEditor.js
+++ jsybuben-client/src/app/TextEditor.js
@@ -13,44 +13,36 @@ define([
         templateString: template,
 
         startup: function() {
-            var sourceTextArea = dom.byId("sourceTextArea");
-            var convertedTextArea = dom.byId("convertedTextArea");
-            var wordListArea = dom.byId("wordListArea");
+            // DO NOTHING
+        },
 
-            var conversionButton = dom.byId("conversionButton");
-            var collectionButton = dom.byId("collectionButton");
-            var persistenceButton = dom.byId("persistenceButton");
+        onConversionButtonClicked: function(e) {
+            var sourceText = this.sourceTextArea.value;
+            var convertedText = jsybuben.to_aybuben(sourceText);
+            this.convertedTextArea.value = convertedText;
+            this.statusMessage.innerHTML = `Converted into text in Aybuben.`
+        },
 
-            var statusMessage = dom.byId("statusMessage");
+        onCollectionButtonClicked: function(e) {
+            var targetText = this.convertedTextArea.value;
+            var wordSet = jsybuben.to_hayeren_word_set(targetText);
+            var text = ""
+            for (let word of wordSet) {
+                text += word + "\n"
+            }
+            this.wordListArea.value = text;
+            var count = wordSet.size;
+            if (count == 0) {
+                this.statusMessage.innerHTML = `No Hayeren words extracted.`
+            } else if (count == 1) {
+                this.statusMessage.innerHTML = `1 Hayeren word extracted.`
+            } else {
+                this.statusMessage.innerHTML = `${count} Hayeren words extracted.`
+            }
+        },
 
-            on(conversionButton, "click", function(e) {
-                var sourceText = sourceTextArea.value;
-                var convertedText = jsybuben.to_aybuben(sourceText);
-                convertedTextArea.value = convertedText;
-                statusMessage.innerHTML = `Converted into text in Aybuben.`
-            });
-
-            on(collectionButton, "click", function(e) {
-                var targetText = convertedTextArea.value;
-                var wordSet = jsybuben.to_hayeren_word_set(targetText);
-                var text = ""
-                for (let word of wordSet) {
-                    text += word + "\n"
-                }
-                wordListArea.value = text;
-                var count = wordSet.size;
-                if (count == 0) {
-                    statusMessage.innerHTML = `No Hayeren words extracted.`
-                } else if (count == 1) {
-                    statusMessage.innerHTML = `1 Hayeren word extracted.`
-                } else {
-                    statusMessage.innerHTML = `${count} Hayeren words extracted.`
-                }
-            });
-            
-            on(persistenceButton, "click", function(e) {
-                statusMessage.innerHTML = "Persistence not implemented yet."
-            });
+        onPersistenceButtonClicked: function(e) {
+            this.statusMessage.innerHTML = "Persistence not implemented yet."
         }
     });
 });
